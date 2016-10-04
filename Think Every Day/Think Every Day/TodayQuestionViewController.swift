6d66cbf37ac80b19e7fb38719fde4aeacbe26492
Coco Sheng <shengkecoco@gmail.com>
2016-10-04 17:37:41 -0400

    little change

--- Think Every Day/Think Every Day/TodayQuestionViewController.swift
+++ Think Every Day/Think Every Day/TodayQuestionViewController.swift
@@ -9,17 +9,19 @@
 import UIKit
 
 class TodayQuestionViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func TodayQ(_ sender: UIButton) {
+        
+    }
 }
 
