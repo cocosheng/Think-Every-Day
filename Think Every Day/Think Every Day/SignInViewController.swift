87f1a7c0f83c50273a58d978e7617039539c8c94
Coco Sheng <shengkecoco@gmail.com>
2016-10-05 00:57:31 -0400

    rough sign in/up done
    can track current user by using callback method, details not implemented

--- Think Every Day/Think Every Day/SignInViewController.swift
+++ Think Every Day/Think Every Day/SignInViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
+import Firebase
+
+@objc(SignInViewController)
 class SignInViewController: UIViewController {
 
     @IBOutlet weak var UsernameSignInTextField: UITextField!
     @IBOutlet weak var PasswordSignInTextField: UITextField!
 
     @IBOutlet weak var AppIconSignIn: UIImageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.AppIconSignIn.image = #imageLiteral(resourceName: "AppIconMainMenu")
@@ -28,6 +31,18 @@ class SignInViewController: UIViewController {
     @IBAction func SignInButtonTapped(_ sender: AnyObject) {
         self.dismiss(animated: true, completion: nil);
         // Change isSignedIn bool to true.
+        // // no need, as firebass provides current user tracking
+        // // can get the user's account data from the user object that's passed to the callback method
+        FIRAuth.auth()?.signIn(withEmail: UsernameSignInTextField.text!, password: PasswordSignInTextField.text!, completion: {
+            user, error in
+            if error != nil{
+                // wrong sign in information
+                print("Incorrect")
+            }
+            else{
+                print("Successful sign in")
+            }
+        })
     }
 
 }
--- Think Every Day/Think Every Day/SignUpViewController.swift
+++ Think Every Day/Think Every Day/SignUpViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
+import Firebase
+
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var UsernameSignUp: UITextField!
+    @IBOutlet weak var PasswordSignUp: UITextField!
+    
+    @IBOutlet weak var PasswordCheckSignUp: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,4 +28,17 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func SignUpButton(_ sender: UIButton) {
+        FIRAuth.auth()?.createUser(withEmail: UsernameSignUp.text!, password: PasswordSignUp.text!, completion: {
+            user, error in
+            if error != nil {
+                // user already existed
+            }
+            else{
+                print("User created")
+                // directly go to main view
+                self.performSegue(withIdentifier: "SignedUp", sender: self)
+            }
+        })
+    }
 }
