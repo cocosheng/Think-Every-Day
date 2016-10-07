3f6081620953d2e5f2b8bed68c0976bc36fd214a
Coco Sheng <shengkecoco@gmail.com>
2016-10-07 17:25:08 -0400

    storage pod installed

---
+++ Think Every Day/Think Every Day/NetworkingService.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkingService.swift
+//  Think Every Day
+//
+//  Created by Ke Sheng on 10/7/16.
+//  Copyright © 2016 TED. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseDatabase
+
+
+struct NetworkingService {
+    
+    let databaseRef = FIRDatabase.database().reference()
+    
+    
+}
